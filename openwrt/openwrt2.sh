31969a64842b331d3502cea11d7a26e90202a685
B-blaow <153058786+B-blaow@users.noreply.github.com>
2026-04-16 01:14:53 +0800

    Update openwrt2.sh

--- openwrt/openwrt2.sh
+++ openwrt/openwrt2.sh
@@ -4,8 +4,11 @@
 sed -i 's/192.168.1.1/192.168.5.1/g' $(pwd)/package/base-files/files/bin/config_generate
 #
 #
-#
-#
-#add nano
-sed -i 's/^# CONFIG_PACKAGE_nano is not set/CONFIG_PACKAGE_nano=y/' .config
-grep -q '^CONFIG_PACKAGE_nano=y' .config || echo 'CONFIG_PACKAGE_nano=y' >> .config
+#cat >> .config <<EOF
+CONFIG_PACKAGE_vlmcsd=y
+CONFIG_PACKAGE_luci-app-vlmcsd=y
+CONFIG_PACKAGE_luci-i18n-vlmcsd-zh-cn=y
+CONFIG_PACKAGE_nano=y
+CONFIG_PACKAGE_iperf3=y
+CONFIG_PACKAGE_htop=y
+EOF
