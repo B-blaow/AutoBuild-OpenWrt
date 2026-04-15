2bb61c54fd725cb182d8bb6580c4eb0ec06fc3c0
B-blaow <153058786+B-blaow@users.noreply.github.com>
2026-04-16 01:08:59 +0800

    Update openwrt1.sh

--- openwrt/openwrt1.sh
+++ openwrt/openwrt1.sh
@@ -11,6 +11,31 @@
 
 # add mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-
-
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+
+
+git clone --depth=1 \
+  https://github.com/coolsnowwolf/luci.git \
+  -b openwrt-25.12 \
+  /tmp/luci
+
+mkdir -p package/luci-app-vlmcsd
+cp -r /tmp/luci/applications/luci-app-vlmcsd/* package/luci-app-vlmcsd/
+
+rm -rf /tmp/luci
+
+#
+git clone --depth=1 \
+  https://github.com/coolsnowwolf/packages.git \
+  -b master \
+  /tmp/package
+
+mkdir -p package/net/vlmcsd
+cp -r /tmp/package/net/vlmcsd/* package/net/vlmcsd/
+
+rm -rf /tmp/package
+
+
+
+#homeproxy
+git clone https://github.com/immortalwrt/homeproxy.git package/homeproxy
