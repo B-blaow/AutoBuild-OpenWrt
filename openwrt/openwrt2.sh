1a85da6c2170fb17fce9744a015d00643855924a
B-blaow <153058786+B-blaow@users.noreply.github.com>
2026-04-17 14:26:12 +0800

    Update openwrt2.sh

--- openwrt/openwrt2.sh
+++ openwrt/openwrt2.sh
@@ -4,7 +4,7 @@
 sed -i 's/192.168.1.1/192.168.5.1/g' $(pwd)/package/base-files/files/bin/config_generate
 #
 #
-#cat >> .config <<EOF
+cat >> .config <<EOF
 CONFIG_PACKAGE_vlmcsd=y
 CONFIG_PACKAGE_luci-app-vlmcsd=y
 CONFIG_PACKAGE_luci-i18n-vlmcsd-zh-cn=y
